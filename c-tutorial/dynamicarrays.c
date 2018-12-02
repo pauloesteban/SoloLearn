498dba054d74da30b11c6f485fdab4ae4640e5fd
Paulo Chiliguano <paulo.chiliguano@gmail.com>
2018-12-02 18:41:16 -0500

    Function for initialize and increase array

--- c-tutorial/dynamicarrays.c
+++ c-tutorial/dynamicarrays.c
@@ -7,19 +7,19 @@ typedef struct {
     int cap;
 } dyn_array;
 
+void init_array(dyn_array *arr);
+void increase_array(dyn_array *arr, int num_elements);
+
 int main() {
     dyn_array arr;
     int i;
 
     /* initialize array */
-    arr.size = 0;
-    arr.elements = calloc(1, sizeof(arr.elements));
-    arr.cap = 1;    /* room for 1 element */
+    init_array(&arr);
+    printf("%d\n", arr.size);
 
     /* expand by 5 more elements */
-    arr.elements = realloc(arr.elements, (5 + arr.cap)*sizeof(arr.elements));
-    if (arr.elements != NULL)
-        arr.cap += 5;   /* increase capacity */
+    increase_array(&arr, 5);
 
     /* add an element and increase size */
     if (arr.size < arr.cap) {
@@ -34,4 +34,16 @@ int main() {
         printf("Element %d: %d\n", i, arr.elements[i]);
 
     return 0;
+}
+
+void init_array(dyn_array *arr) {
+    arr->size = 0;
+    arr->elements = calloc(1, sizeof(arr->elements));
+    arr->cap = 1;    /* room for 1 element */
+}
+
+void increase_array(dyn_array *arr, int num_elements) {
+    arr->elements = realloc(arr->elements, (num_elements + arr->cap)*sizeof(arr->elements));
+    if (arr->elements != NULL)
+        arr->cap += num_elements;
 }
